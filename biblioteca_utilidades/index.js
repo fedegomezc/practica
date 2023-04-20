ce50ff855d5f7f8bb08d7b814a95feded2aaf816
Federico Gomez Conde <fedeic8@gmail.com>
2023-04-20 12:02:08 -0300

    use Lodash methods

--- biblioteca_utilidades/index.js
+++ biblioteca_utilidades/index.js
@@ -1,3 +1,5 @@
+import _ from "lodash"
+
 const entries = [
     {
       titulo: "Introducción a HTML5",
@@ -57,4 +59,22 @@ const entries = [
     },
   ];
 
-  console.log(entries)
+
+// consigna:  Utiliza los métodos de Lodash para Filtrar las entradas que tenga el tag de 'Desarrollo' , Mapea por titulo y Agrupa por autor.
+
+// filtrado por las entradas con el tag 'Desarrollo'.
+// _.filter(collection, [predicate=_.identity])     Itera sobra una colección y devuelve un array con los valores que resultan truthy en el segundo parámetro (predicate)
+// en nuestro caso invocamos una funcion que debe recorrer el array de la propiedad 'tags' y utilizamos _.includes para buscar coincidencia con el tag 'Desarrollo'
+const filterByDeveloperTag = _.filter(entries, function(entry){
+    return _.includes(entry.tags, "Desarrollo")
+})
+
+// mapeo por titulo
+//_.map(collection, [iteratee=_.identity])      Crea un array de elementos que resulta de aplicar la función del segundo parametro en cada elemento.
+const mapByTitle = _.map(entries, "titulo")
+
+// agrupar por autor
+// _.groupBy(collection, [iteratee=_.identity])    Crea un objeto que agrupa por keys los valores correspondientes según lo que se indique en el segundo parámetro.
+const groupByAuthor = _.groupBy(entries, "autor")
+
+console.log(groupByAuthor)
